1e58dbbc68e4b166ff24e3dd0e2e096b5f900e17
Daniel <prof.danial4@gmail.com>
2025-03-22 12:33:53 +0330

    Domain/Modify debug objects

--- AlibabaClone.Domain/obj/Debug/net8.0/AlibabaClone.Domain.AssemblyInfo.cs
+++ AlibabaClone.Domain/obj/Debug/net8.0/AlibabaClone.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlibabaClone.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fc19a02cd7412aa77029324b3c958a29ce30f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlibabaClone.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlibabaClone.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

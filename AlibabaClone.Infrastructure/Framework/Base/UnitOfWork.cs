5e19d248147dd60f39cf3254e67a25a5a45ff354
Daniel <prof.danial4@gmail.com>
2025-03-28 20:16:16 +0330

    Infrastructure/Update framework

---
+++ AlibabaClone.Infrastructure/Framework/Base/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using AlibabaClone.Domain.Framework.Interfaces;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories;
+using AlibabaClone.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Infrastructure.Framework.Base
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDBContext _context;
+
+        public UnitOfWork (ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose ()
+        {
+            _context.Dispose ();
+        }
+
+        public async Task<int> SaveChangesAsync ()
+        {
+            return await _context.SaveChangesAsync ();
+        }
+    }
+}

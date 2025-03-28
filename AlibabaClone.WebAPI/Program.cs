7e9abc36578002c8a4ab31084f3bc621fffb8428
Daniel <prof.danial4@gmail.com>
2025-03-28 19:53:18 +0330

    WebAPI/Update program startup

--- AlibabaClone.WebAPI/Program.cs
+++ AlibabaClone.WebAPI/Program.cs
@@ -1,25 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AlibabaClone.Infrastructure;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder (args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add the database context
+builder.Services.AddDbContext<ApplicationDBContext> (options =>
+    options.UseSqlServer (builder.Configuration.GetConnectionString ("DefaultConnection")));
 
-var app = builder.Build();
+builder.Services.AddScoped (typeof (IRepository<>), typeof (Repository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork> ();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+var app = builder.Build ();
+app.Run ();

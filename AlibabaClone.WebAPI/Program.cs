7495e7fe73de7d22330214c540203347906c9a0b
Daniel <prof.danial4@gmail.com>
2025-03-28 20:23:09 +0330

    WebAPI/Update program startup

--- AlibabaClone.WebAPI/Program.cs
+++ AlibabaClone.WebAPI/Program.cs
@@ -1,17 +1,64 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using AlibabaClone.Domain.Framework.Interfaces;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories.AccountRepositories;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories.CompanyRepositories;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories.LocationRepositories;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories.TransactionRepositories;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories.TransportationRepositories;
+using AlibabaClone.Domain.Framework.Interfaces.Repositories.VehicleRepositories;
 using AlibabaClone.Infrastructure;
+using AlibabaClone.Infrastructure.Framework.Base;
+using AlibabaClone.Infrastructure.Services.AccountAggregates;
+using AlibabaClone.Infrastructure.Services.CompanyAggregates;
+using AlibabaClone.Infrastructure.Services.TransactionAggregates;
+using AlibabaClone.Infrastructure.Services.TransportationAggregates;
+using AlibabaClone.Infrastructure.Services.VehicleAggregates;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder (args);
 
-// Add the database context
-builder.Services.AddDbContext<ApplicationDBContext> (options =>
-    options.UseSqlServer (builder.Configuration.GetConnectionString ("DefaultConnection")));
+builder.Services.AddControllers ();
+builder.Services.AddEndpointsApiExplorer ();
+builder.Services.AddSwaggerGen ();
 
-builder.Services.AddScoped (typeof (IRepository<>), typeof (Repository<>));
+var connectionString = builder.Configuration.GetConnectionString ("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDBContext> (options => options.UseSqlServer (connectionString));
+
+builder.Services.AddScoped<IAccountRepository, AccountRepository> ();
+builder.Services.AddScoped<IGenderRepository, GenderRepository> ();
+builder.Services.AddScoped<IPersonRepository, PersonRepository> ();
+builder.Services.AddScoped<IRoleRepository, RoleRepository> ();
+
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository> ();
+
+builder.Services.AddScoped<ICityRepository, CityRepository> ();
+builder.Services.AddScoped<ILocationRepository, LocationRepository> ();
+builder.Services.AddScoped<ILocationTypeRepository, LocationTypeRepository> ();
+
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository> ();
+
+builder.Services.AddScoped<ITicketRepository, TicketRepository> ();
+builder.Services.AddScoped<ITicketStatusRepository, TicketStatusRepository> ();
+builder.Services.AddScoped<ITransportationRepository, TransportationRepository> ();
+
+builder.Services.AddScoped<ISeatRepository, SeatRepository> ();
+builder.Services.AddScoped<IVehicleRepository, VehicleRepository> ();
+builder.Services.AddScoped<IVehicleTypeRepository, VehicleTypeRepository> ();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork> ();
 
+
 var app = builder.Build ();
+
+if (app.Environment.IsDevelopment ())
+{
+    app.UseSwagger ();
+    app.UseSwaggerUI ();
+}
+
+app.UseHttpsRedirection ();
+
+app.UseAuthorization ();
+
+app.MapControllers ();
+
 app.Run ();

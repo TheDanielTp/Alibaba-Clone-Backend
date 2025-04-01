425b202565d84713181e23d4cf8d673eb8ba6a38
Daniel <prof.danial4@gmail.com>
2025-04-01 13:28:41 +0330

    Domain/Update repository interface

--- AlibabaClone.Domain/Framework/Interfaces/Repositories/IRepository.cs
+++ AlibabaClone.Domain/Framework/Interfaces/Repositories/IRepository.cs
@@ -7,8 +7,8 @@ namespace AlibabaClone.Domain.Framework.Interfaces.Repositories
         Task<T_Entity?> GetByIdAsync (U_PrimaryKey id);
         Task<IEnumerable<T_Entity>> GetAllAsync ();
         Task<IEnumerable<T_Entity>> FindAsync (Expression<Func<T_Entity, bool>> predicate);
-        Task AddAsync (T_Entity entity);
 
+        Task AddAsync (T_Entity entity);
         void Update (T_Entity entity);
         void Remove (T_Entity entity);
     }

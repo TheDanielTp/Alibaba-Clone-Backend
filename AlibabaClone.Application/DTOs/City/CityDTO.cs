e1373b322d23b98bcfc4e11a0ad3fe72aa6bf673
Daniel <prof.danial4@gmail.com>
2025-04-11 11:03:55 +0330

    Application/Add city & transportation DTOs

---
+++ AlibabaClone.Application/DTOs/City/CityDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AlibabaClone.Application.DTOs.City
+{
+    public class CityDto
+    {
+        public int Id { get; init; }
+        public required string Title { get; init; }
+    }
+}
---
+++ AlibabaClone.Application/DTOs/Transportation/TransportationSearchResultDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace AlibabaClone.Application.DTOs.Transportation
+{
+    public class TransportationSearchResultDto
+    {
+        public long Id { get; init; }
+        public required string CompanyTitle { get; init; }
+        public required string FromLocationTitle { get; init; }
+        public required string ToLocationTitle { get; init; }
+        public required string FromCityTitle { get; init; }
+        public required string ToCityTitle { get; init; }
+        public DateTime StartDateTime { get; init; }
+        public DateTime? EndDateTime { get; init; }
+        public decimal Price { get; init; }
+    }
+}

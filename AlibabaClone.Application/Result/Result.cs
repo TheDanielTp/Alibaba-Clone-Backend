985acf05726692c366b8b316e488f7cf4b976010
Daniel <prof.danial4@gmail.com>
2025-04-30 09:57:27 +0330

    Application/Initialize result

---
+++ AlibabaClone.Application/Result/Result.cs
@@ -0,0 +1,37 @@
+﻿namespace AlibabaClone.Application.Result
+{
+    public class Result<T>
+    {
+        public ResultStatus Status { get; set; }
+        public string? ErrorMessage { get; set; }
+        public T? Data { get; set; }
+        public bool IsSuccess => Status == ResultStatus.Success;
+
+        public static Result<T> Success (T data)
+        {
+            return new Result<T>
+            {
+                Status = ResultStatus.Success,
+                Data = data
+            };
+        }
+
+        public static Result<T> Error (T data)
+        {
+            return new Result<T>
+            {
+                Status = ResultStatus.Error,
+                Data = data
+            };
+        }
+
+        public static Result<T> NotFound (T data)
+        {
+            return new Result<T>
+            {
+                Status = ResultStatus.NotFound,
+                Data = data
+            };
+        }
+    }
+}

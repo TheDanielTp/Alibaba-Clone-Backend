02384ab3d23805fcc253bdc0f8acaeafce84bfcc
Daniel <prof.danial4@gmail.com>
2025-04-30 10:04:24 +0330

    WebAPI/Add controllers

---
+++ AlibabaClone.WebAPI/Controllers/CityController.cs
@@ -0,0 +1,33 @@
+﻿using AlibabaClone.Application.DTOs.Transportation;
+using AlibabaClone.Application.Interfaces;
+using AlibabaClone.Application.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlibabaClone.WebAPI.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly ICityService _cityService;
+        public CityController (ICityService cityService)
+        {
+            _cityService = cityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCities ()
+        {
+            var result = await _cityService.GetCitiesAsync ();
+            if (result.IsSuccess)
+                return Ok (result.Data);
+
+            return result.Status switch
+            {
+                ResultStatus.NotFound => NotFound (result.ErrorMessage),
+                ResultStatus.ValidationError => BadRequest (result.ErrorMessage),
+                _ => StatusCode (500, result.ErrorMessage)
+            };
+        }
+    }
+}

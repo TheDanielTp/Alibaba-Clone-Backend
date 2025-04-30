4dee90bd9f151b8b10e4ad62e24442057e2f6eaf
Daniel <prof.danial4@gmail.com>
2025-04-30 09:56:36 +0330

    Application/Add mapping profile

---
+++ AlibabaClone.Application/Mappers/Profiles/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AlibabaClone.Application.DTOs.City;
+using AlibabaClone.Application.DTOs.Transportation;
+using AlibabaClone.Domain.Aggregates.LocationAggregates;
+using AlibabaClone.Domain.Aggregates.TransportationAggregates;
+using AutoMapper;   
+
+namespace AlibabaClone.Application.Mappers.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile ()
+        {
+            CreateMap<Transportation, TransportationSearchResultDto> ()
+                .ForMember (dest => dest.CompanyTitle,
+                opt => opt.MapFrom (src => src.Company.Title))
+
+                .ForMember (dest => dest.FromLocationTitle,
+                opt => opt.MapFrom (src => src.FromLocation.Title))
+
+                .ForMember (dest => dest.ToLocationTitle,
+                opt => opt.MapFrom (src => src.ToLocation.Title))
+
+                .ForMember (dest => dest.FromCityTitle,
+                opt => opt.MapFrom (src => src.FromLocation.City.Title))
+
+                 .ForMember (dest => dest.ToCityTitle,
+                 opt => opt.MapFrom (src => src.ToLocation.City.Title));
+
+            CreateMap<City, CityDto> ();
+        }
+    }
+}

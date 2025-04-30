873e6b9fe7347802ed2141c2b2dec9b287643131
Daniel <prof.danial4@gmail.com>
2025-04-30 09:55:39 +0330

    Application/Add service interfaces

---
+++ AlibabaClone.Application/Interfaces/ICityService.cs
@@ -0,0 +1,15 @@
+﻿using AlibabaClone.Application.DTOs.City;
+using AlibabaClone.Application.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Application.Interfaces
+{
+    public interface ICityService
+    {
+        Task<Result<IEnumerable<CityDto>>> GetCitiesAsync ();
+    }
+}
---
+++ AlibabaClone.Application/Interfaces/ITransportationService.cs
@@ -0,0 +1,15 @@
+﻿using AlibabaClone.Application.DTOs.Transportation;
+using AlibabaClone.Application.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlibabaClone.Application.Interfaces
+{
+    public interface ITransportationService
+    {
+        Task<Result<IEnumerable<TransportationSearchResultDto>>> SearchTransportationsAsync (TransportationSearchRequestDto searchRequest);
+    }
+}
